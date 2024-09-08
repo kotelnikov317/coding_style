2cf862c45ea9567130b2ef1ff58a2033b3904c77
Anatolyi Kotelnikov <kotelnikov_git@mail.ru>
2024-09-08 20:05:10 +0500

    modified:   .gitignore 		add file documentation 	new file:   Doxyfile		settings documentation 	modified:   mycomplex.cpp 	documentation 	modified:   mycomplex.h		documentation

--- mycomplex.cpp
+++ mycomplex.cpp
@@ -1,37 +1,71 @@
+/*!
+    @file mycomplex.cpp
+    @brief Файл с описанием методов класса Complex
+*/
+
 #include <iostream>
 #include <cmath>
 #include "mycomplex.h"
 
 using namespace std;
 
+/*!
+    @brief Конструктор класса
+    @param aRe Действительная составляющая
+    @param aIm Мнимая составляющая
+*/
 Complex::Complex(double aRe, double aIm){
     Re = aRe;
     Im = aIm;
 }
 
+/*!
+    @brief Конструктор копирования класса Complex
+    @param aRval Комплексное число
+*/
 Complex::Complex(const Complex &aRval){
     Re = aRval.Re;
     Im = aRval.Im;
 }
 
+/*!
+    @brief Деструктор класса
+*/
 Complex::~Complex(){
     Re = 0.0;
     Im = 0.0;
 }
 
+/*!
+    @brief Ввод комплексого числа
+    @param aRe Действительная часть
+    @param aIm Мнимая часть
+*/
 void Complex::Set(double aRe,double aIm){
     Re = aRe;
     Im = aIm;
 }
 
+/*!
+    @brief Возвращает модуль комплексного числа
+    @return Модуль комплексного числа
+*/
 Complex::operator double(){
     return abs();
 }
 
+/*!
+    @brief Возвращает модуль комплексного числа
+*/
 double Complex::abs(){
     return sqrt(Re * Re + Im * Im);
 }
 
+/*!
+    @brief Оператор сложения
+    @param aRval Комплесное число
+    @result Результат сложения
+*/
 Complex Complex::operator+(const Complex &aRval){
     Complex Result;
     Result.Re = Re + aRval.Re;
@@ -39,6 +73,11 @@ Complex Complex::operator+(const Complex &aRval){
     return Result;
 }
 
+/*!
+    @brief Оператор вычитания
+    @param aRval Комплесное число
+    @result Результат вычитания
+*/
 Complex Complex::operator-(const Complex &aRval){
     Complex Result;
     Result.Re = Re - aRval.Re;
@@ -46,6 +85,11 @@ Complex Complex::operator-(const Complex &aRval){
     return Result;
 }
 
+/*!
+    @brief Оператор сложения действительной части
+    @param aval Действительная часть
+    @result Результат сложения
+*/
 Complex Complex::operator+(const double &aval){
     Complex result;
     result.Re = Re + aval;
@@ -53,12 +97,22 @@ Complex Complex::operator+(const double &aval){
     return result;
 }
 
+/*!
+    @brief Оператор вычитания
+    @param aRval Действительная часть
+    @result Результат вычитания
+*/
 Complex Complex::operator-(const double &aRval){
     Complex Result(*this);
     Result.Re = Re - aRval;
     return Result;
 }
 
+/*!
+    @brief Оператор умножения
+    @param aRval Комплексное число
+    @result Результат умножения
+*/
 Complex Complex::operator*(const Complex &aRval ){
     Complex Result;
     Result.Re = Re * aRval.Re - Im * aRval.Im;
@@ -66,6 +120,11 @@ Complex Complex::operator*(const Complex &aRval ){
     return Result;
 }
 
+/*!
+    @brief Оператор умножения
+    @param aRval Действительная часть
+    @result Результат умножения
+*/
 Complex Complex::operator*(const double &aRval){
     Complex Result;
     Result.Re = Re * aRval;
@@ -73,6 +132,11 @@ Complex Complex::operator*(const double &aRval){
     return Result;
 }
 
+/*!
+    @brief Оператор деления
+    @param aRval Действительная часть
+    @result Результат деления
+*/
 Complex Complex::operator/(const double &aRval){
     Complex Result;
     Result.Re = Re / aRval;
@@ -80,18 +144,33 @@ Complex Complex::operator/(const double &aRval){
     return Result;
 }
 
+/*!
+    @brief Оператор сложения
+    @param arval Комплексное число
+    @result Результат сложения
+*/
 Complex &Complex::operator+=(const Complex &arval){
     Re += arval.Re;
     Im += arval.Im;
     return *this;
 }
 
+/*!
+    @brief Оператор вычитания
+    @param aRval Комплексное число
+    @result Результат вычитания
+*/
 Complex &Complex::operator-=(const Complex &aRval){
     Re -= aRval.Re;
     Im -= aRval.Im;
     return *this;
 }
 
+/*!
+    @brief Оператор умножения
+    @param aRval Комплексное число
+    @result Результат умножения
+*/
 Complex &Complex::operator*=(const Complex &aRval){
     double tmpRe = Re;
     Re = Re * aRval.Re - Im * aRval.Im;
@@ -99,46 +178,88 @@ Complex &Complex::operator*=(const Complex &aRval){
     return *this;
 }
 
+/*!
+    @brief Оператор сложения
+    @param aRval Действительная часть
+    @result Результат сложения
+*/
 Complex &Complex::operator+=(const double &aRval){
     Re += aRval;
     return *this;
 }
 
+/*!
+    @brief Оператор вычитания
+    @param aRval Действительная часть
+    @result Результат вычитания
+*/
 Complex &Complex::operator-=(const double &aRval){
     Re -= aRval;
     return *this;
 }
 
+/*!
+    @brief Оператор умножения
+    @param aRval Действительная часть
+    @result Результат умножения
+*/
 Complex &Complex::operator*=(const  double  &aRval){
     Re *= aRval;
     Im *= aRval;
     return *this;
 }
 
+/*!
+    @brief Оператор деления
+    @param aRval Действительная часть
+    @result Результат деления
+*/
 Complex &Complex::operator/=(const double &aRval){
     Re /= aRval;
     Im /= aRval;
     return *this;
 }
 
+/*!
+    @brief Оператор присвоения
+    @param aRval Комплексное число
+    @result Результат присвоения
+*/
 Complex &Complex::operator=(const Complex& aRval){
     Re = aRval.Re;
     Im = aRval.Im;
     return *this;
 }
 
+/*!
+    @brief Оператор присвоения
+    @param aRval Действительная часть
+    @result Результат присвоения
+*/
 Complex &Complex::operator=(const double& aRval){
     Re = aRval;
     Im = 0.0;
     return *this;
 }
 
+/*!
+    @brief Оператор ввода комплексного чилса в поток
+    @param stream Входной поток
+    @param a Комплексное число для ввода
+    @result Входной поток после операции ввода
+*/
 istream &operator>>(istream &stream, Complex&a){
     char tmp[256];
     stream >> a.Re >> a.Im >> tmp;
     return stream;
 }
 
+/*!
+    @brief Оператор вывода комплексного чилса из потока
+    @param stream Выходной поток
+    @param a Комплексное число для вывода
+    @result Выходной поток после операции вывода
+*/
 ostream &operator<<(ostream &stream, Complex&a){
     stream << a.Re;
     if(!(a.Im < 0))
@@ -147,6 +268,12 @@ ostream &operator<<(ostream &stream, Complex&a){
     return stream;
 }
 
+/*!
+    @brief Оператор сложения
+    @param aLval Действительная часть
+    @param aRval Комплексное число
+    @result Результат сложения
+*/
 Complex operator+(const double &aLval,const Complex &aRval){
     Complex Result;
     Result.Re = aLval + aRval.Re;
@@ -154,6 +281,12 @@ Complex operator+(const double &aLval,const Complex &aRval){
     return Result;
 }
 
+/*!
+    @brief Оператор вычитания
+    @param aLval Действительная часть
+    @param aRval Комплексное число
+    @result Результат вычитания
+*/
 Complex operator-(const double &aLval,const Complex&aRval){
     Complex Result;
     Result.Re = aLval - aRval.Re;
@@ -161,9 +294,15 @@ Complex operator-(const double &aLval,const Complex&aRval){
     return Result;
 }
 
+/*!
+    @brief Оператор умножения
+    @param aLval Действительная часть
+    @param aRval Комплексное число
+    @result Результат умножения
+*/
 Complex operator*(const double &aLval,const Complex &a){
     Complex r;
     r.Re = aLval * a.Re;
     r.Im = aLval * a.Im;
-    return r;
+    return Result;
 }
--- mycomplex.h
+++ mycomplex.h
@@ -1,39 +1,191 @@
+/*!
+    @file mycomplex.h
+    @brief Заголовочный файл класса Complex
+*/
+
 #ifndef _MY_COMPLEX_H_
 #define _MY_COMPLEX_H_
 #include <iostream>
 
 using namespace std;
 
+/*!
+    @brief Класс комплексного числа
+*/
 class Complex{
-	public:
+        /*!
+            @brief Действительная составляющая
+        */
         double Re;
+        /*!
+            @brief Мнимая составляющая
+        */
         double Im;
+	public:
+	    /*!
+            @brief Возвращает модуль комплексного числа
+        */
         double abs();
+
+	    /*!
+            @brief Конструктор класса Complex
+            @param aRe Действительная часть
+            @param aIm Мнимая часть
+	    */
         Complex(double aRe = 0, double aIm = 0);
+
+        /*!
+            @brief Конструктор копирования класса Complex
+            @param Комплексное число
+        */
         Complex(const Complex&);
+
+        /*!
+            @brief Деструктор класса
+        */
         ~Complex();
+
+        /*!
+            @brief Ввод комплексого числа
+            @param aRe Действительная часть
+            @param aIm Мнимая часть
+        */
         void Set(double aRe, double aIm=0);
+
+        /*!
+            @brief Возвращает модуль комплексного числа
+        */
         operator double();
+
+        /*!
+            @brief Оператор ввода комплексного чилса в поток
+            @param Входной поток
+            @param Комплексное число для ввода
+        */
         friend istream &operator>>(istream&, Complex&);
+
+        /*!
+            @brief Оператор вывода комплексного чилса из потока
+            @param Выходной поток
+            @param Комплексное число для вывода
+        */
         friend ostream &operator<<(ostream&, Complex&);
+
+        /*!
+            @brief Оператор сложения
+            @param Действительная часть
+            @param Комплексное число
+        */
         friend Complex operator+(const double&, const Complex&);
+
+        /*!
+            @brief Оператор вычитания
+            @param Действительная часть
+            @param Комплексное число
+        */
         friend Complex operator-(const double&, const Complex&);
+
+        /*!
+            @brief Оператор умножения
+            @param Действительная часть
+            @param Комплексное число
+        */
         friend Complex operator*(const double&, const Complex&);
+
+        /*!
+            @brief Оператор сложения
+            @param Действительная часть
+        */
         Complex operator+(const double&);
+
+        /*!
+            @brief Оператор вычитания
+            @param Действительная часть
+        */
         Complex operator-(const double&);
+
+        /*!
+            @brief Оператор умножения
+            @param Действительная часть
+        */
         Complex operator*(const double&);
+
+        /*!
+            @brief Оператор деления
+            @param Действительная часть
+        */
         Complex operator/(const double&);
+
+        /*!
+            @brief Оператор сложения
+            @param Комплексное число
+        */
         Complex operator+(const Complex&);
+
+        /*!
+            @brief Оператор вычитания
+            @param Комплексное число
+        */
         Complex operator-(const Complex&);
+
+        /*!
+            @brief Оператор умножения
+            @param Комплексное число
+        */
         Complex operator*(const Complex&);
+
+        /*!
+            @brief Оператор присвоения
+            @param Комплексное число
+        */
         Complex &operator=(const Complex&);
+
+        /*!
+            @brief Оператор сложения
+            @param Комплексное число
+        */
         Complex &operator+=(const Complex&);
+
+        /*!
+            @brief Оператор вычитания
+            @param Комплексное число
+        */
         Complex &operator-=(const Complex&);
+
+        /*!
+            @brief Оператор умножения
+            @param Комплексное число
+        */
         Complex &operator*=(const Complex&);
+
+        /*!
+            @brief Оператор присвоения
+            @param Действительная часть
+        */
         Complex &operator=(const double&);
+
+        /*!
+            @brief Оператор сложения
+            @param Действительная часть
+        */
         Complex &operator+=(const double&);
+
+        /*!
+            @brief Оператор вычитания
+            @param Действительная часть
+        */
         Complex &operator-=(const double&);
+
+        /*!
+            @brief Оператор умножения
+            @param Действительная часть
+        */
         Complex &operator*=(const double&);
+
+        /*!
+            @brief Оператор деления
+            @param Действительная часть
+        */
         Complex &operator/=(const double&);
 };
 #endif
